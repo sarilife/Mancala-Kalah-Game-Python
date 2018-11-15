99d22449e9a63f2639551664461b622019364704
Sari <sarilife3@gmail.com>
2018-11-15 10:25:53 +0200

    Kalah class and hole class 1

--- mancala.py
+++ mancala.py
@@ -1,14 +1,22 @@
 
-#
-# class hole :
-#     def __init__(self,hole_id,seeds_num):
-#         self.hole_id = hole_id
-#         self.seeds_num = seeds_num
+class Hole :
+    def __init__(self,hole_id,seeds_num):
+        self.hole_id = hole_id
+        self.seeds_num = seeds_num
 
 class Kalah:
     def __init__(self,holes,seeds):
-        self.holes = holes*2+2 #(i,)for i in range(1,holes)]
-        self.seeds = seeds*self.holes
+        self.holes_number = holes*2+2 #(i,)for i in range(1,holes)]
+        self.seeds_number = seeds * self.holes_number
+        self.holes = [Hole(i,seeds) for i in range(1,self.holes_number+1)]
+
 
     def play(self,hole):
         pass
+
+
+# k = Kalah(6,4)
+# # print(k.holes)
+#
+# for i in range(len(k.holes)):
+#     print(k.holes[i].hole_id,k.holes[i].seeds_num)
